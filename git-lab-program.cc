047028b2c7f536500e03b638718924e47d6ebfd4
Carley Brackney <122946619+CarleyJB@users.noreply.github.com>
2023-01-24 19:37:13 -0500

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Carley Brackney
+ *        Date: 1/24/23
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
